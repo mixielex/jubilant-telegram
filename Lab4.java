12f92c857d785db6b44061a15f0d0935b848314e
mixielex <36489446+mixielex@users.noreply.github.com>
2018-02-14 16:52:57 -0500

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+//Alexandra Atlas
+import java.util.Scanner; //new comment!
 public class Main {
     public static void main(String args[]) {
         Scanner myScanner = new Scanner(System.in);
